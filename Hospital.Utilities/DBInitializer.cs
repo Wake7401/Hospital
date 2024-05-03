06bdc24ac9de1d9b570d9033ac8cb5cb100da9ea
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2024-05-03 10:24:00 +0700

    Add website role

---
+++ Hospital.Utilities/DBInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Hospital.Responsitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Hospital.Utilities
+{
+    public class DBInitializer : IDbInitializer
+    {
+        private UserManager<IdentityUser> __userManager;
+        private RoleManager<IdentityRole> __roleManager;
+        private ApplicationDbContext __context;
+
+        public DBInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager,ApplicationDbContext context)
+        {
+            __userManager = userManager;
+            __roleManager = roleManager;
+            __context = context;
+        }
+        public void Initialize() {
+            throw new NotImplementedException();
+        }
+    }
+}
